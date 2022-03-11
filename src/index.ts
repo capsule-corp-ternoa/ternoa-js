480c786aa974d4ed7ff7476e15dd5f967576cb8d
ghalielouarzazi@hotmail.com <ghalielouarzazi@hotmail.com>
2022-03-11 15:44:29 +0100

    modularize functions

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
-import { getQueryAndSubscribe, isTransactionSuccess, runTransaction } from "./functions/blockchain"
-import { getKeyringFromSeed } from "./functions/account"
 import { unFormatBalance } from "./utils/polkadot"
-import { chainQuery, txActions, txPallets } from "./constants"
 import type { ISubmittableResult } from "@polkadot/types/types"
+import { chainQuery, txActions, txPallets } from "./constants"
+import * as account from "./functions/account"
+import * as blockchain from "./functions/blockchain"
 
-const quickTest = async () => {
+const test = async () => {
   try {
     //test transaction
-    const keyring = await getKeyringFromSeed(
+    const keyring = await account.getKeyringFromSeed(
       "soccer traffic version fault humor tackle bid tape obvious wild fish coin",
     )
-    const hash = await runTransaction(
+    const hash = await blockchain.runTransaction(
       txPallets.balances,
       txActions.transfer,
       ["5CDGXH8Q9DzD3TnATTG6qm6f4yR1kbECBGUmh2XbEBQ8Jfa5", await unFormatBalance(1)],
       keyring,
       (res: ISubmittableResult) => {
         if (res.status.isInBlock) {
-          const isSuccess = isTransactionSuccess(res)
+          const isSuccess = blockchain.isTransactionSuccess(res)
           console.log(res.status.toHuman())
           console.log(isSuccess)
         }
@@ -26,7 +26,7 @@ const quickTest = async () => {
     console.log("hash", hash)
 
     //test query with subscribe
-    await getQueryAndSubscribe(
+    await blockchain.getQueryAndSubscribe(
       txPallets.system,
       chainQuery.account,
       ["5CDGXH8Q9DzD3TnATTG6qm6f4yR1kbECBGUmh2XbEBQ8Jfa5"],
@@ -37,7 +37,10 @@ const quickTest = async () => {
   }
 }
 
-quickTest()
+test()
 
-export * from "./functions/account"
-export * from "./functions/blockchain"
+export * as account from "./functions/account"
+export * as blockchain from "./functions/blockchain"
+export * as capsule from "./functions/capsule"
+export * as marketplace from "./functions/marketplace"
+export * as nft from "./functions/nft"
