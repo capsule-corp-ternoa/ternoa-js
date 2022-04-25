8c8eea430ff6fc670e9fa645ab0d7030f6a2c3ea
Igor Papandinas <igor.papandinas@posteo.net>
2022-04-25 12:56:46 +0200

    task: Fees tests designed

--- src/fee/index.test.ts
+++ src/fee/index.test.ts
@@ -1,6 +1,17 @@
-// getTransactionFeesEstimate should return fees for simple transaction (eg:; balance transfer)
-// getTransactionTreasury fee should return fees for valid transaction (create / createNFT)
-// getTransactionTreasury fee should return 0 for transaction different than create / createFromNft
-test("no empty test suite", async () => {
-  expect(true)
+describe("Checking funds to pay transaction gas fees on tx submit", (): void => {
+  it("Should throw an error if insufficient funds for gas or treasury", async () => {
+    // create new account with generateSeed
+    // create TX of a balance transfer
+    // signed it with the new account
+    // reject with Error
+  })
+})
+
+describe("Testing fees getters", (): void => {
+  it("Should get the gas fee estimation for a transaction", async () => {
+    // use testing pair
+    // create TX of a balance transfer
+    // signed it with the new account
+    // expect to get BN
+  })
 })
