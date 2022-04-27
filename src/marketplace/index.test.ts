945142010df48e8c62b7975c4aba28f44f74c377
Victor SALOMON <victor-salomon@hotmail.fr>
2022-04-27 12:06:14 +0200

    TJ-25 fix: remove unused code

--- src/marketplace/index.test.ts
+++ src/marketplace/index.test.ts
@@ -2,19 +2,6 @@ import { isBN } from "bn.js"
 import { checkBalanceToCreateMarketplace, getMarketplaceMintFee } from "."
 import { generateSeed } from "../account"
 
-describe("Testing account balance and marketplace fees to mint marketplace", (): void => {
-  xit("Insufficient funds should throw an Error: 'Insufficient funds to create a marketplace", async (): Promise<void> => {
-    const account = await generateSeed()
-    await expect(checkBalanceToCreateMarketplace(account.address)).rejects.toThrow(
-      Error("Insufficient funds to create a marketplace"),
-    )
-  })
-  xit("Should retrun the fee as a BN to mint a marketplace", async () => {
-    const marketplaceMintFee = await getMarketplaceMintFee()
-    expect(isBN(marketplaceMintFee)).toBe(true)
-  })
-})
-
 describe("Testing marketplace creation", (): void => {
   describe("Testing account balance and marketplace fees to mint marketplace", (): void => {
     xit("Insufficient funds should throw an Error: 'Insufficient funds to create a marketplace", async (): Promise<void> => {
