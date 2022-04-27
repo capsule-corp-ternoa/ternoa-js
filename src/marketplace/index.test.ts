d388b410c81237879bcb1037a89a02af68a8a1a6
Victor SALOMON <victor-salomon@hotmail.fr>
2022-04-27 12:04:11 +0200

    TJ-25 add tests to check marketplace mint fee

--- src/marketplace/index.test.ts
+++ src/marketplace/index.test.ts
@@ -1,7 +1,34 @@
 import { isBN } from "bn.js"
-import { getMarketplaceMintFee } from "."
+import { checkBalanceToCreateMarketplace, getMarketplaceMintFee } from "."
+import { generateSeed } from "../account"
 
-test.skip("Checking the fees to mint a marketplace", async () => {
-  const marketplaceMintFee = await getMarketplaceMintFee()
-  expect(isBN(marketplaceMintFee)).toBe(true)
+describe("Testing account balance and marketplace fees to mint marketplace", (): void => {
+  xit("Insufficient funds should throw an Error: 'Insufficient funds to create a marketplace", async (): Promise<void> => {
+    const account = await generateSeed()
+    await expect(checkBalanceToCreateMarketplace(account.address)).rejects.toThrow(
+      Error("Insufficient funds to create a marketplace"),
+    )
+  })
+  xit("Should retrun the fee as a BN to mint a marketplace", async () => {
+    const marketplaceMintFee = await getMarketplaceMintFee()
+    expect(isBN(marketplaceMintFee)).toBe(true)
+  })
+})
+
+describe("Testing marketplace creation", (): void => {
+  describe("Testing account balance and marketplace fees to mint marketplace", (): void => {
+    xit("Insufficient funds should throw an Error: 'Insufficient funds to create a marketplace", async (): Promise<void> => {
+      const account = await generateSeed()
+      await expect(checkBalanceToCreateMarketplace(account.address)).rejects.toThrow(
+        Error("Insufficient funds to create a marketplace"),
+      )
+    })
+    xit("Should retrun the fee as a BN to mint a marketplace", async () => {
+      const marketplaceMintFee = await getMarketplaceMintFee()
+      expect(isBN(marketplaceMintFee)).toBe(true)
+    })
+  })
+  xit("Should return a correct minted marketplace hash hex or message ", async (): Promise<void> => {
+    //to be tested
+  })
 })
