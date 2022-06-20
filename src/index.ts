7aa2b3d6e5538c508bc43212111ad18855b4e55d
Igor Papandinas <igor.papandinas@posteo.net>
2022-06-20 20:01:05 +0200

    feat(JS-126): hexToString/hexToU8a/stringToHex/u8aToHex utils exported from polkadot

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,5 @@ export * from "./capsule"
 export * from "./fee"
 export * from "./marketplace"
 export * from "./nft"
+
+export { hexToString, hexToU8a, stringToHex, u8aToHex } from "@polkadot/util"
