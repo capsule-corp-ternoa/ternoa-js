0d14be30bb4b2747e5c7e0db3459ed3d8a840980
Marko Petrlic <petrlic.marko@gmail.com>
2022-07-07 10:25:12 +0200

    Readded doc for NFT transactions.

--- src/nft/extrinsics.ts
+++ src/nft/extrinsics.ts
@@ -17,7 +17,15 @@ import { formatRoyalty } from "./storage"
 
 // NFTs
 
-/// TODO DOC!
+/**
+ * @name createNftTx
+ * @summary             Creates an unsigned unsubmitted Create-NFT Transaction Hash.
+ * @param offchainData  Off-chain related NFT metadata. Can be an IPFS Hash, an URL or plain text.
+ * @param royalty       Percentage of all second sales that the creator will receive. It's a decimal number in range [0, 100]. Default is 0.
+ * @param collectionId  The collection that this NFT will belong. Optional Parameter.
+ * @param isSoulbound   If true makes the NFT untransferable. Default is false.
+ * @returns             Unsigned unsubmitted Create-NFT Transaction Hash. The Hash is only valid for 5 minutes.
+ */
 export const createNftTx = async (
   offchainData: string,
   royalty = 0,
@@ -33,7 +41,17 @@ export const createNftTx = async (
   ])
 }
 
-/// TODO DOC!
+/**
+ * @name createNft
+ * @summary             Creates an NFT on the chain.
+ * @param offchainData  Off-chain related NFT metadata. Can be an IPFS Hash, an URL or plain text.
+ * @param royalty       Percentage of all second sales that the creator will receive. It's a decimal number in range [0, 100]. Default is 0.
+ * @param collectionId  The collection that this NFT will belong. Optional Parameter.
+ * @param isSoulbound   If true makes the NFT untransferable. Default is false.
+ * @param keyring       Account that will sign the transaction.
+ * @param waitUntil     Execution trigger that can be set either to BlockInclusion or BlockFinalization.
+ * @returns             NFTCreatedEvent Blockchain event.
+ */
 export const createNft = async (
   offchainData: string,
   royalty = 0,
@@ -47,24 +65,50 @@ export const createNft = async (
   return events.findEventOrThrow(NFTCreatedEvent)
 }
 
-/// TODO DOC!
+/**
+ * @name burnNftTx
+ * @summary   Creates an unsigned unsubmitted Burn-NFT Transaction Hash.
+ * @param id  The ID of the NFT.
+ * @returns   Unsigned unsubmitted Burn-NFT Transaction Hash. The Hash is only valid for 5 minutes.
+ */
 export const burnNftTx = async (id: number): Promise<TransactionHash> => {
   return await createTxHex(txPallets.nft, txActions.burnNft, [id])
 }
 
-/// TODO DOC!
+/**
+ * @name burnNft
+ * @summary           Burns an NFT from the chain.
+ * @param id          The ID of the NFT.
+ * @param keyring     Account that will sign the transaction.
+ * @param waitUntil   Execution trigger that can be set either to BlockInclusion or BlockFinalization.
+ * @returns           NFTBurnedEvent Blockchain event.
+ */
 export const burnNft = async (id: number, keyring: IKeyringPair, waitUntil: WaitUntil): Promise<NFTBurnedEvent> => {
   const tx = await burnNftTx(id)
   const events = await submitTxBlocking(tx, waitUntil, keyring)
   return events.findEventOrThrow(NFTBurnedEvent)
 }
 
-/// TODO DOC!
+/**
+ * @name delegateNftTx
+ * @summary           Creates an unsigned unsubmitted Delegate-NFT Transaction Hash.
+ * @param id          The ID of the NFT.
+ * @param recipient   Destination account.
+ * @returns           Unsigned unsubmitted Delegate-NFT Transaction Hash. The Hash is only valid for 5 minutes.
+ */
 export const delegateNftTx = async (id: number, recipient: string | undefined = undefined): Promise<TransactionHash> => {
   return await createTxHex(txPallets.nft, txActions.delegateNft, [id, recipient])
 }
 
-/// TODO DOC!
+/**
+ * @name delegateNft
+ * @summary           Delegates an NFT to someone.
+ * @param id          The ID of the NFT.
+ * @param recipient   Destination account.
+ * @param keyring     Account that will sign the transaction.
+ * @param waitUntil   Execution trigger that can be set either to BlockInclusion or BlockFinalization.
+ * @returns           NFTDelegatedEvent Blockchain event.
+ */
 export const delegateNft = async (
   id: number,
   recipient: string | undefined = undefined,
@@ -76,13 +120,27 @@ export const delegateNft = async (
   return events.findEventOrThrow(NFTDelegatedEvent)
 }
 
-/// TODO DOC!
+/**
+ * @name setRoyaltyTx
+ * @summary       Creates an unsigned unsubmitted Set-Royalty Transaction Hash.
+ * @param id      The ID of the NFT.
+ * @param amount  The new royalty value.
+ * @returns       Unsigned unsubmitted Set-Royalty-NFT Transaction Hash. The Hash is only valid for 5 minutes.
+ */
 export const setRoyaltyTx = async (id: number, amount: number): Promise<TransactionHash> => {
   const formatedRoyalty = await formatRoyalty(amount)
   return await createTxHex(txPallets.nft, txActions.setRoyalty, [id, formatedRoyalty])
 }
 
-/// TODO DOC!
+/**
+ * @name setRoyalty
+ * @summary           Sets the royalty of an NFT.
+ * @param id          The ID of the NFT.
+ * @param amount      The new royalty value.
+ * @param keyring     Account that will sign the transaction.
+ * @param waitUntil   Execution trigger that can be set either to BlockInclusion or BlockFinalization.
+ * @returns           NFTRoyaltySetEvent Blockchain event.
+ */
 export const setRoyalty = async (
   id: number,
   amount: number,
@@ -94,12 +152,26 @@ export const setRoyalty = async (
   return events.findEventOrThrow(NFTRoyaltySetEvent)
 }
 
-/// TODO DOC!
+/**
+ * @name transferNftTx
+ * @summary           Creates an unsigned unsubmitted Transfer-NFT Transaction Hash.
+ * @param id          The ID of the NFT.
+ * @param recipient   Destination account.
+ * @returns           Unsigned unsubmitted Transfer-NFT Transaction Hash. The Hash is only valid for 5 minutes
+ */
 export const transferNftTx = async (id: number, recipient: string): Promise<TransactionHash> => {
   return await createTxHex(txPallets.nft, txActions.transferNft, [id, recipient])
 }
 
-/// TODO DOC!
+/**
+ * @name transferNft
+ * @summary           Sends on our NFTs to someone.
+ * @param id          The ID of the NFT.
+ * @param number      Destination account.
+ * @param keyring     Account that will sign the transaction.
+ * @param waitUntil   Execution trigger that can be set either to BlockInclusion or BlockFinalization.
+ * @returns           NFTTransferredEvent Blockchain event.
+ */
 export const transferNft = async (
   id: number,
   number: string,
@@ -111,12 +183,26 @@ export const transferNft = async (
   return events.findEventOrThrow(NFTTransferredEvent)
 }
 
-/// TODO DOC!
+/**
+ * @name addNftToCollectionTx
+ * @summary               Creates an unsigned unsubmitted Add-NFT-To-Collection Transaction Hash.
+ * @param nft_id          The ID of the NFT.
+ * @param collection_id   The ID of the Collection.
+ * @returns               Unsigned unsubmitted Add-NFT-To-Collection Transaction Hash. The Hash is only valid for 5 minutes.
+ */
 export const addNftToCollectionTx = async (nft_id: number, collection_id: number): Promise<TransactionHash> => {
   return await createTxHex(txPallets.nft, txActions.addNftToCollection, [nft_id, collection_id])
 }
 
-/// TODO DOC!
+/**
+ * @name addNftToCollection
+ * @summary               Adds an NFT to an existing collection.
+ * @param nft_id          The ID of the NFT.
+ * @param collection_id   The ID of the Collection.
+ * @param keyring         Account that will sign the transaction.
+ * @param waitUntil       Execution trigger that can be set either to BlockInclusion or BlockFinalization.
+ * @returns               NFTAddedToCollectionEvent Blockchain event.
+ */
 export const addNftToCollection = async (
   nft_id: number,
   collection_id: number,
@@ -130,7 +216,13 @@ export const addNftToCollection = async (
 
 // Collections
 
-/// TODO DOC!
+/**
+ * @name createCollectionTx
+ * @summary               Creates an unsigned unsubmitted Create-Collection Transaction Hash.
+ * @param offchainData    Off-chain related Collection metadata. Can be an IPFS Hash, an URL or plain text.
+ * @param limit           The maximum amount that NFTs that the collection can hold. This is optional
+ * @returns               Unsigned unsubmitted Create-Collection Transaction Hash. The Hash is only valid for 5 minutes.
+ */
 export const createCollectionTx = async (
   offchainData: string,
   limit: number | undefined = undefined,
@@ -138,7 +230,15 @@ export const createCollectionTx = async (
   return await createTxHex(txPallets.nft, txActions.createCollection, [offchainData, limit])
 }
 
-/// TODO DOC!
+/**
+ * @name createCollection
+ * @summary               Creates a collection.
+ * @param offchainData    Off-chain related Collection metadata. Can be an IPFS Hash, an URL or plain text.
+ * @param limit           Amount of NFTs that can be associated with this collection. This is optional
+ * @param keyring         Account that will sign the transaction.
+ * @param waitUntil       Execution trigger that can be set either to BlockInclusion or BlockFinalization.
+ * @returns               CollectionCreatedEvent Blockchain event.
+ */
 export const createCollection = async (
   offchainData: string,
   limit: number | undefined = undefined,
@@ -150,12 +250,26 @@ export const createCollection = async (
   return events.findEventOrThrow(CollectionCreatedEvent)
 }
 
-/// TODO DOC!
+/**
+ * @name limitCollectionTx
+ * @summary       Creates an unsigned unsubmitted Limit-Collection Transaction Hash.
+ * @param id      The ID of the Collection.
+ * @param limit   Amount of NFTs that can be associated with this collection.
+ * @returns       Unsigned unsubmitted Limit-Collection Transaction Hash. The Hash is only valid for 5 minutes.
+ */
 export const limitCollectionTx = async (id: number, limit: number): Promise<TransactionHash> => {
   return await createTxHex(txPallets.nft, txActions.limitCollection, [id, limit])
 }
 
-/// TODO DOC!
+/**
+ * @name limitCollection
+ * @summary           Limits how many NFTs can be associated with this collection.
+ * @param id          The ID of the Collection.
+ * @param limit       Amount of NFTs that can be associated with this collection.
+ * @param keyring     Account that will sign the transaction.
+ * @param waitUntil   Execution trigger that can be set either to BlockInclusion or BlockFinalization.
+ * @returns           CollectionLimitedEvent Blockchain event.
+ */
 export const limitCollection = async (
   id: number,
   limit: number,
@@ -167,12 +281,24 @@ export const limitCollection = async (
   return events.findEventOrThrow(CollectionLimitedEvent)
 }
 
-/// TODO DOC!
+/**
+ * @name closeCollectionTx
+ * @summary   Creates an unsigned unsubmitted Close-Collection Transaction Hash.
+ * @param id  The ID of the Collection.
+ * @returns   Unsigned unsubmitted Close-Collection Transaction Hash. The Hash is only valid for 5 minutes.
+ */
 export const closeCollectionTx = async (id: number): Promise<TransactionHash> => {
   return await createTxHex(txPallets.nft, txActions.closeCollection, [id])
 }
 
-/// TODO DOC!
+/**
+ * @name closeCollection
+ * @summary           Closes the collection so that no new NFTs can be added.
+ * @param id          The ID of the Collection.
+ * @param keyring     Account that will sign the transaction.
+ * @param waitUntil   Execution trigger that can be set either to BlockInclusion or BlockFinalization.
+ * @returns           CollectionClosedEvent Blockchain event.
+ */
 export const closeCollection = async (
   id: number,
   keyring: IKeyringPair,
@@ -183,12 +309,24 @@ export const closeCollection = async (
   return events.findEventOrThrow(CollectionClosedEvent)
 }
 
-/// TODO DOC!
+/**
+ * @name burnCollectionTx
+ * @summary   Creates an unsigned unsubmitted Burn-Collection Transaction Hash.
+ * @param id  The ID of the Collection.
+ * @returns   Unsigned unsubmitted Burn-Collection Transaction Hash. The Hash is only valid for 5 minutes.
+ */
 export const burnCollectionTx = async (id: number): Promise<TransactionHash> => {
   return await createTxHex(txPallets.nft, txActions.burnCollection, [id])
 }
 
-/// TODO DOC!
+/**
+ * @name burnCollection
+ * @summary           Burns an existing collection. The collections needs to be empty before it can be burned.
+ * @param id          The ID of the Collection.
+ * @param keyring     Account that will sign the transaction.
+ * @param waitUntil   Execution trigger that can be set either to BlockInclusion or BlockFinalization.
+ * @returns           CollectionBurnedEvent Blockchain event.
+ */
 export const burnCollection = async (
   id: number,
   keyring: IKeyringPair,
