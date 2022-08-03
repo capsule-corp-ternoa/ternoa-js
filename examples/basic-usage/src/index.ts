753f758eba8455dd1114f1f16299f39187af349c
Marko Petrlic <petrlic.marko@gmail.com>
2022-08-03 15:48:15 +0200

    Small changes

--- examples/basic-usage/src/index.ts
+++ examples/basic-usage/src/index.ts
@@ -11,7 +11,7 @@ async function main() {
   // This will initialize the internal SDK API.
   //
   // It's not strictly necessary but it's good practice to initialize the API as soon as possible.
-  // If this call is omitted then the first SDK call will return an expection.
+  // If this call is omitted then the first SDK call will return an exception.
   // You can also specify the endpoint by passing the endpoint address as the first argument.
   await initializeApi()
 
--- examples/starter-project/src/index.ts
+++ examples/starter-project/src/index.ts
@@ -5,12 +5,15 @@ async function main() {
   // This will initialize the internal SDK API.
   //
   // It's not strictly necessary but it's good practice to initialize the API as soon as possible.
-  // If this call is omitted then the first SDK call will return an expection.
+  // If this call is omitted then the first SDK call will return an exception.
   // You can also specify the endpoint by passing the endpoint address as the first argument.
+  console.log("Connecting...");
   await initializeApi();
 
+  console.log("Disconnecting...");
   await safeDisconnect();
-  
+
+  console.log("Done :)");
   process.exit()
 }
 
