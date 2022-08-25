382087be730e60a5da23f8a74eaf1d7a096a4060
Victor SALOMON <victor-salomon@hotmail.fr>
2022-08-25 14:36:15 +0200

    added the dateToBlockNumber function

--- src/utils.ts
+++ src/utils.ts
@@ -13,8 +13,20 @@ export const blockNumberToDate = async (blockNumber: number) => {
   const signedBlock = await api.rpc.chain.getBlock(blockHash)
   const timestampNow = signedBlock.block.extrinsics[0].method.args
   const formatedTimestamp = timestampNow.toString().slice(0, -3)
-  const date = new Date(Number(formatedTimestamp) * 1000).toLocaleString()
+  const date = new Date(Number(formatedTimestamp) * 1000).toLocaleString() // toLocaleString ou pas ?
   return date
+}
+
+export const dateToBlockNumber = async (date: Date) => {
+  // date : toLocaleString ou pas ?
+  const today = new Date() // toLocaleString ou pas ?
+  const duration = date.getTime() - today.getTime()
+  const numberOfBlocks = await msDurationToBlockNumber(duration)
+  const api = getRawApi()
+  const lastBlockDatas = await api.rpc.chain.getBlock()
+  const lastBlockNumber = Number(lastBlockDatas.block.header.number.toString())
+  const blockNumber = Math.ceil(numberOfBlocks + lastBlockNumber) // math ceil ou pas ?
+  return blockNumber
 }
 
 export const blockNumberToMsDuration = async (blockNumber: number, duration: number | undefined = 6) => {
