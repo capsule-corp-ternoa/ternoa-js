a310d5a7df230913083dffdb5401bc96e95f8663
Victor SALOMON <victor-salomon@hotmail.fr>
2022-08-25 10:36:00 +0200

    added the blockNumberToDate utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,18 @@
 import BN from "bn.js"
+import { Errors } from "./constants"
 
-import { balanceToNumber } from "./blockchain"
+import { balanceToNumber, getRawApi } from "./blockchain"
 
 export const roundBalance = (amount: string) =>
   Number(balanceToNumber(new BN(amount), { forceUnit: "-", withUnit: false }).split(",").join(""))
+
+export const blockNumberToDate = async (blockNumber: number) => {
+  if (blockNumber <= 0) throw new Error(Errors.VALUE_LOWER_THAN_0)
+  const api = getRawApi()
+  const blockHash = await api.rpc.chain.getBlockHash(blockNumber)
+  const signedBlock = await api.rpc.chain.getBlock(blockHash)
+  const timestampNow = signedBlock.block.extrinsics[0].method.args
+  const formatedTimestamp = timestampNow.toString().slice(0, -3)
+  const date = new Date(Number(formatedTimestamp) * 1000).toLocaleString()
+  return date
+}
