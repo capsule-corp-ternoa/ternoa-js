9a696fafb31c895df85a88854d79bfa30537d53e
Victor SALOMON <victor-salomon@hotmail.fr>
2022-09-05 12:00:33 +0200

    fixed cancellation rentee event and added util

--- src/rent/utils.ts
+++ src/rent/utils.ts
@@ -1,23 +1,26 @@
 import { numberToBalance } from "../blockchain"
+import { CancellationFeeType, RentFeeType } from "./types"
 /**
  * @name formatRentFee
- * @summary         Checks the type fee and format it accordingly. Numbers are formatted into BN when RentFeeType is a Token .
- * @param fee       The fee to format : It can only be an flexibleTokensFee or CancellationFeeType.
+ * @summary         Checks the type fee and format it accordingly. Numbers are formatted into BN.
+ * @param fee       The fee to format : It can only be a RentFeeType or CancellationFeeType.
  * @returns         The formatted fee.
  */
 export const formatRentContractFee = async (fee: any) => {
-  //flexibleTokensFee || CancellationFeeType in stead of any in params
-  if (typeof fee.tokens === "number") {
-    const TokensFee = await numberToBalance(fee.token)
-    fee.tokens = TokensFee
-  }
-  if (typeof fee.fixedTokens === "number") {
-    const fixedTokensFee = await numberToBalance(fee.fixedTokens)
-    fee.fixedTokens = fixedTokensFee
-  }
-  if (typeof fee.flexibleTokens === "number") {
-    const flexibleTokensFee = await numberToBalance(fee.flexibleTokens)
-    fee.flexibleTokens = flexibleTokensFee
+  //RentFeeType | CancellationFeeType instead of any in params
+  if (typeof fee === "object") {
+    if (typeof fee.tokens === "number") {
+      const TokensFee = await numberToBalance(fee.tokens)
+      fee.tokens = TokensFee
+    }
+    if (typeof fee.fixedTokens === "number") {
+      const fixedTokensFee = await numberToBalance(fee.fixedTokens)
+      fee.fixedTokens = fixedTokensFee
+    }
+    if (typeof fee.flexibleTokens === "number") {
+      const flexibleTokensFee = await numberToBalance(fee.flexibleTokens)
+      fee.flexibleTokens = flexibleTokensFee
+    }
   }
   return fee
 }
