a2ad3ddf2e0bca25460b2c7a9e9339013b04469e
Victor SALOMON <victor-salomon@hotmail.fr>
2022-09-20 15:23:42 +0200

    added assets pallet extrinsics

---
+++ src/assets/extrinsics.ts
@@ -0,0 +1,45 @@
+import BN from "bn.js"
+import type { IKeyringPair } from "@polkadot/types/types"
+
+import { txActions, txPallets, WaitUntil } from "../constants"
+import { createTxHex, numberToBalance, submitTxBlocking, TransactionHashType } from "../blockchain"
+import { AssetTransferredEvent } from "../events"
+
+/**
+ * @name assetTransferTx
+ * @summary             Creates an unsigned unsubmitted Assets-Transfert Transaction Hash.
+ * @param id            ID of the Asset
+ * @param target        Public address of the account to transfer the amount to.
+ * @param amount        Token amount to transfer.
+ * @returns             Unsigned unsubmitted Assets-Transfert Transaction Hash. The Hash is only valid for 5 minutes.
+ */
+export const assetTransferTx = async (
+  id: number,
+  target: string,
+  amount: number | BN,
+): Promise<TransactionHashType> => {
+  const transferredAmount = typeof amount === "number" ? await numberToBalance(amount) : amount
+  return await createTxHex(txPallets.assets, txActions.transfer, [id, target, transferredAmount])
+}
+
+/**
+ * @name assetTransfer
+ * @summary             Transfers some balance to another account.
+ * @param id            ID of the Asset
+ * @param target        Public address of the account to transfer the amount to.
+ * @param amount        Token amount to transfer.
+ * @param keyring       Account that will sign the transaction.
+ * @param waitUntil     Execution trigger that can be set either to BlockInclusion or BlockFinalization.
+ * @returns             AssetTransferredEvent Blockchain event.
+ */
+export const assetTransfer = async (
+  id: number,
+  target: string,
+  amount: number | BN,
+  keyring: IKeyringPair,
+  waitUntil: WaitUntil,
+): Promise<AssetTransferredEvent> => {
+  const tx = await assetTransferTx(id, target, amount)
+  const events = await submitTxBlocking(tx, waitUntil, keyring)
+  return events.findEventOrThrow(AssetTransferredEvent)
+}
