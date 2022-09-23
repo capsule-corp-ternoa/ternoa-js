33a7ff293d011fdefdf5fd94a82d06f1c3a6e3f6
Igor Papandinas <igor.papandinas@posteo.net>
2022-09-23 12:19:48 +0200

    task: Main index imports improved #106

--- src/_misc/scripts/frank-build.mjs
+++ src/_misc/scripts/frank-build.mjs
@@ -18,9 +18,11 @@ async function createPackageFile() {
         assets: ["./assets/index.d.ts"],
         balance: ["./balance/index.d.ts"],
         blockchain: ["./blockchain/index.d.ts"],
+        helpers: ["./helpers/index.d.ts"],
         nft: ["./nft/index.d.ts"],
         marketplace: ["./marketplace/index.d.ts"],
-        constants: ["./constants/index.d.ts"],
+        events: ["./events.d.ts"],
+        constants: ["./constants.d.ts"],
       },
     },
   }
