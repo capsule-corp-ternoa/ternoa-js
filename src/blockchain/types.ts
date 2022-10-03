714e81b2431fe8e9e74daf1fd78a4efb99ed9e7e
Victor SALOMON <victor-salomon@hotmail.fr>
2022-10-03 13:06:54 +0200

    added check batch helpers #110

--- src/blockchain/types.ts
+++ src/blockchain/types.ts
@@ -1,4 +1,5 @@
 import BN from "bn.js"
+import { BatchInterruptedEvent, BlockchainEvents, ExtrinsicFailedEvent, ItemFailedEvent } from "../events"
 
 export interface IFormatBalanceOptions {
   /**
@@ -29,3 +30,19 @@ export interface IFormatBalanceOptions {
 
 export type TransactionHashType = `0x${string}`
 export type BalanceType = BN
+
+export type CheckTransactionType = {
+  isTxSuccess: boolean
+  events?: BlockchainEvents
+  failedEvent?: ExtrinsicFailedEvent
+}
+export interface ICheckBatch extends CheckTransactionType {
+  isBatchInterrupted: boolean
+  indexInterrupted?: number
+  batchInterruptedEvent?: BatchInterruptedEvent
+}
+
+export interface ICheckForceBatch extends CheckTransactionType {
+  isBatchCompleteWithoutErrors?: boolean
+  failedItems?: ItemFailedEvent[]
+}
