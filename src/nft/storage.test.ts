f0b15c8d6a1233b9cceaa18893ed945ba560fe9d
dhammani <drishammani@gmail.com>
2022-10-31 14:18:31 +0100

    refacto unit test

--- src/nft/storage.test.ts
+++ src/nft/storage.test.ts
@@ -10,12 +10,13 @@ import {
   getSecretNftOffchainData,
 } from "./storage"
 import { createTestPairs } from "../_misc/testingPairs"
-import { createCollection, createNft } from "./extrinsics"
+import { createCollection, createNft, createSecretNft } from "./extrinsics"
 import { WaitUntil } from "../constants"
 
 const TEST_DATA = {
   collectionId: 0,
   nftId: 0,
+  secretNftId: 0,
 }
 
 beforeAll(async () => {
@@ -26,8 +27,11 @@ beforeAll(async () => {
   const { test: testAccount } = await createTestPairs()
   const cEvent = await createCollection("Collection Test", undefined, testAccount, WaitUntil.BlockInclusion)
   const nEvent = await createNft("Test NFT Data", 0, cEvent.collectionId, false, testAccount, WaitUntil.BlockInclusion)
+  const secretNft = await createSecretNft("Test NFT Data", "Test Secret NFT Data", 0, undefined, false, testAccount, WaitUntil.BlockInclusion)
+
   TEST_DATA.collectionId = cEvent.collectionId
   TEST_DATA.nftId = nEvent.nftId
+  TEST_DATA.secretNftId = secretNft.nftId
 })
 
 it("NFT Mint Fee storage should exist and it should not be null", async () => {
@@ -66,7 +70,7 @@ describe("Testing getting NFT data", (): void => {
     expect(maybeNft != null).toBe(true)
   })
   it("Should return secret offchain data when the NFT ID exists and is a Secret NFT", async () => {
-    const actual = await getSecretNftOffchainData(96)
+    const actual = await getSecretNftOffchainData(TEST_DATA.secretNftId)
     expect(actual != undefined).toBe(true)
   })
 })
