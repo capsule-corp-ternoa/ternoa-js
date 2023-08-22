260ca5f24d9436793e572cea6404d1b7cd5d3d6c
Victor-Salomon <victor-salomon@hotmail.fr>
2023-08-22 15:10:44 +0200

    remove era from submitMetricsServerReportTx

--- src/tee/extrinsics.ts
+++ src/tee/extrinsics.ts
@@ -7,27 +7,20 @@ import { MetricsServerReportSubmittedEvent } from "../events"
 /**
  * @name submitMetricsServerReportTx
  * @summary                         Creates an unsigned unsubmitted Submit Metrics Server Report Transaction Hash for an Era.
- * @param eraIndex                  The era to which scores belongs.
  * @param operatorAddress           The operator address to which submitted scores belongs.
  * @param metricsServerReport       The report containing the 5 scores computed for the mentioned era and the submitter's registered address.
  * @returns                         Unsigned unsubmitted Submit Metrics Server Report Transaction Hash. The Hash is only valid for 5 minutes.
  */
 export const submitMetricsServerReportTx = async (
-  eraIndex: number | undefined,
   operatorAddress: string,
   metricsServerReport: ReportParamsType,
 ): Promise<TransactionHashType> => {
-  return await createTxHex(txPallets.tee, txActions.submitMetricsServerReport, [
-    eraIndex,
-    operatorAddress,
-    metricsServerReport,
-  ])
+  return await createTxHex(txPallets.tee, txActions.submitMetricsServerReport, [operatorAddress, metricsServerReport])
 }
 
 /**
  * @name submitMetricsServerReport
  * @summary                         Submit the metrics server report for a specific era.
- * @param eraIndex                  The era to which scores belongs.
  * @param operatorAddress           The operator address to which submitted scores belongs.
  * @param metricsServerReport       The report containing the 5 scores computed for the mentioned era and the submitter's registered address.
  * @param keyring                   Account that will sign the transaction.
@@ -35,13 +28,12 @@ export const submitMetricsServerReportTx = async (
  * @returns                         MetricsServerReportSubmittedEvent Blockchain event.
  */
 export const submitMetricsServerReport = async (
-  eraIndex: number | undefined,
   operatorAddress: string,
   metricsServerReport: ReportParamsType,
   keyring: IKeyringPair,
   waitUntil: WaitUntil,
 ): Promise<MetricsServerReportSubmittedEvent> => {
-  const tx = await submitMetricsServerReportTx(eraIndex, operatorAddress, metricsServerReport)
+  const tx = await submitMetricsServerReportTx(operatorAddress, metricsServerReport)
   const { events } = await submitTxBlocking(tx, waitUntil, keyring)
   return events.findEventOrThrow(MetricsServerReportSubmittedEvent)
 }
