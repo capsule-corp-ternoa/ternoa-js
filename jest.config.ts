fdc43274de0b5ef427a332e83fe36180da8692e2
Victor-Salomon <victor-salomon@hotmail.fr>
2023-12-15 15:03:05 +0100

    wip - remove babel, upgrade package and change jest config

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "jest"
+
+export default async (): Promise<Config> => {
+  return {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    detectOpenHandles: true,
+    silent: true,
+    forceExit: true,
+    // isSerial:true, 
+  }
+}
