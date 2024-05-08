90ddbe43345058ff23c485487dc4be8f20cae475
Mohsin Riaz <bahrianboy@gmail.com>
2024-05-08 23:22:17 +0500

    retry ability for uploading keyshares made prepareAndStoreKeyShares into smaller function which uses prepareStoreablePayloadForShares this can be used to prepare shares to be stored with signing
    added identifyAndStoreFailedShares which takes data from prepareAndStoreKeyShares and retry with only failed items
    
    naming convention improvememnts

--- src/helpers/nft.ts
+++ src/helpers/nft.ts
@@ -9,12 +9,12 @@ import {
   formatStorePayload,
   formatRetrievePayload,
   generateKeyShares,
-  getEnclaveHealthStatus,
+  getClusterHealthStatus,
   teeKeySharesRetrieve,
   teeKeySharesStore,
   SIGNER_BLOCK_VALIDITY,
 } from "./tee"
-import { CapsuleMedia, MediaMetadataType, NftMetadataType, PGPKeysType, RequesterType } from "./types"
+import { CapsuleMedia, MediaMetadataType, NftMetadataType, PGPKeysType, RequesterType, StorePayloadType, TeeSharesStoreType } from "./types"
 import { getLastBlock, getSignatureFromExtension, getSignatureFromKeyring } from "./crypto"
 
 import { getKeyringFromSeed } from "../account"
@@ -77,22 +77,44 @@ export const prepareAndStoreKeyShares = async (
   kind: "secret" | "capsule",
   extensionInjector?: Record<string, any>,
   clusterId = 0,
-) => {
+): Promise<TeeSharesStoreType[]> => {
   if (kind !== "secret" && kind !== "capsule") {
     throw new Error(`${Errors.TEE_UPLOAD_ERROR} : Kind must be either "secret" or "capsule"`)
   }
+  // 0. generate secret shares from the private key
+  const shares = generateKeyShares(privateKey)
+  // 1. prepare uploadable payload from shares
+  const payloads = await prepareStoreablePayloadForShares(shares, signer, nftId, extensionInjector)
+  // 2. request to store a batch of secret shares to the enclave
+  return await teeKeySharesStore(clusterId, kind, payloads)
+}
+
+
+/**
+ * @name prepareAndStoreKeyShares
+ * @summary                  Takes the shares and prepares the store payload with signatures of owner and authSigner.
+ * @param shares             The private key to be splited with Shamir algorithm.
+ * @param signer             Account owner of the shares (keyring) or address (string) .
+ * @param nftId              The Capsule NFT id or Secret NFT id to link to the private key.
+ * @param extensionInjector  (Optional)The signer method retrived from your extension to sign the transaction. We recommand Polkadot extention: object must have a signer key.
+ * @returns                  The payload which can be uploaded to enclaves.
+ */
+export const prepareStoreablePayloadForShares = async (
+  shares: string[],
+  signer: IKeyringPair | string,
+  nftId: number,
+  extensionInjector?: Record<string, any>,
+): Promise<StorePayloadType[]> => {
+
   if (typeof signer === "string" && !extensionInjector)
     throw new Error(
       `${Errors.TEE_UPLOAD_ERROR} - INJECTOR_SIGNER_MISSING : injectorSigner must be provided when signer is of type string`,
     )
   if (typeof signer === "string" && !isValidAddress(signer)) throw new Error("INVALID_ADDRESS_FORMAT")
-
   // 0. retrieve last chain block number and generate a temporary signer account with soft-derivation
   const lastBlockId = await getLastBlock()
   const tmpSignerPair = await getTemporarySignerKeys(typeof signer === "string" ? signer : signer.address, lastBlockId)
-  // 1. generate secret shares from the private key
-  const shares = generateKeyShares(privateKey)
-  // 2. format payloads with temporary signer account
+  // 1. format payloads with temporary signer account
   const signerAddress = typeof signer === "string" ? signer : signer.address
   const authMessage = `<Bytes>${tmpSignerPair.address}_${lastBlockId}_${SIGNER_BLOCK_VALIDITY}</Bytes>`
   const authSignature =
@@ -103,13 +125,65 @@ export const prepareAndStoreKeyShares = async (
     throw new Error(
       `${Errors.TEE_UPLOAD_ERROR} : signing of the temporary authentication message failed when uploading payload`,
     )
+  // 2. prepare the payload in format which is storageable on enclaves
   const payloads = await Promise.all(
     shares.map((share: string) =>
       formatStorePayload(signerAddress, authMessage, authSignature, tmpSignerPair, nftId, share, lastBlockId),
     ),
   )
-  // 3. request to store a batch of secret shares to the enclave
-  return await teeKeySharesStore(clusterId, kind, payloads)
+
+  return payloads
+}
+
+/**
+ * @name identifyAndStoreFailedShares
+ * @summary                                    Helper function which takes the response of prepareAndStoreKeyShares and try to upload to failed enclaves. it should only be used when some shares were successfully stored on a clustor hence the clustor id cannot be changed
+ * @param prepareAndStoreKeySharesResponse     Response of prepareAndStoreKeyShares with failed data
+ * @param signer                               Account owner of the nft or address (string) .
+ * @param extensionInjector                    (Optional)The signer method retrived from your extension to sign the transaction. We recommand Polkadot extention: object must have a signer key.
+ * @returns                                    The TEE enclave response (shards datas and description).
+ */
+export const identifyAndStoreFailedShares = async (
+  prepareAndStoreKeySharesResponse: TeeSharesStoreType[],
+  signer: IKeyringPair | string,
+  extensionInjector?: Record<string, any>,
+) => {
+  if (typeof signer === "string" && !extensionInjector)
+    throw new Error(
+      `${Errors.TEE_UPLOAD_ERROR} - INJECTOR_SIGNER_MISSING : injectorSigner must be provided when signer is of type string`,
+    )
+  if (typeof signer === "string" && !isValidAddress(signer)) throw new Error("INVALID_ADDRESS_FORMAT")
+
+  //0. get details from response, these should be available even on errored response
+  const clusterId = prepareAndStoreKeySharesResponse[0].clusterId;
+  const kind: "secret" | "capsule" = prepareAndStoreKeySharesResponse[0].kind;
+  const nftId: number = prepareAndStoreKeySharesResponse[0].nft_id;
+  const nftOwner: string = prepareAndStoreKeySharesResponse[0].owner_address;
+
+  //1. check is current signer is the owner of nft because if nft is not synced the owenr cannot change
+  if (typeof signer === "string") {
+    if (signer !== nftOwner) throw new Error("Signer is not the owner of NFT");
+  } else {
+    if (signer.address !== nftOwner) throw new Error("Signer is not the owner of NFT");
+  }
+
+  const shares: string[] = []
+  const enclaveSlots: number[] = [];
+  //2. get failed shares and the respective enclave slots
+  prepareAndStoreKeySharesResponse.forEach(r => {
+    if (r.isError) {
+      shares.push(r.share)
+      enclaveSlots.push(r.enclaveSlot)
+    }
+  })
+  //if no failed shares, throw error
+  if (shares.length < 1) throw new Error("No faild responses found in the input data");
+
+  //3. prepare the shares again with signing the data
+  const payloads = await prepareStoreablePayloadForShares(shares, signer, nftId, extensionInjector);
+
+  //4. request to store failed shares to the respective enclaves
+  return await teeKeySharesStore(clusterId, kind, payloads, 2, enclaveSlots)
 }
 
 /**
@@ -141,8 +215,8 @@ export const mintSecretNFT = async (
   isSoulbound = false,
   waitUntil = WaitUntil.BlockInclusion,
 ) => {
-  // 0. query Enclave with /Health API
-  await getEnclaveHealthStatus(clusterId)
+  // 0. query Cluster with /Health API
+  await getClusterHealthStatus(clusterId)
 
   // 1. media encryption and upload
   const { privateKey, publicKey } = await generatePGPKeys()
@@ -200,8 +274,8 @@ export const viewSecretNFT = async (
   extensionInjector?: Record<string, any>,
   clusterId = 0,
 ) => {
-  // 0. query Enclave with /Health API
-  await getEnclaveHealthStatus(clusterId)
+  // 0. query Cluster with /Health API
+  await getClusterHealthStatus(clusterId)
 
   // 1. Get Secret NFT metadata hash
   const secretNftOffchainData = await getSecretNftOffchainData(nftId)
@@ -261,8 +335,8 @@ export const mintCapsuleNFT = async (
   isSoulbound = false,
   waitUntil = WaitUntil.BlockInclusion,
 ) => {
-  // 0. query Enclave with /Health API
-  await getEnclaveHealthStatus(clusterId)
+  // 0. query Cluster with /Health API
+  await getClusterHealthStatus(clusterId)
 
   // 1. media encryption and upload
   const { Hash: offchainDataHash } = await ipfsClient.storeNFT(nftFile, nftMetadata)
@@ -315,8 +389,8 @@ export const getCapsuleNFTPrivateKey = async (
   extensionInjector?: Record<string, any>,
   clusterId = 0,
 ): Promise<string> => {
-  // 0. query Enclave with /Health API
-  await getEnclaveHealthStatus(clusterId)
+  // 0. query Cluster with /Health API
+  await getClusterHealthStatus(clusterId)
   // 1. Format and retrieve payload
   const lastBlockId = await getLastBlock()
   const payload = await formatRetrievePayload(
