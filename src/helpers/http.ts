b455fee5fe2be1070218348fce0b07e9cc5e327d
Mohsin Riaz <bahrianboy@gmail.com>
2024-05-08 23:18:45 +0500

    improved error object throw new error removed the error object which contain actual error details from server

--- src/helpers/http.ts
+++ src/helpers/http.ts
@@ -12,14 +12,20 @@ export class HttpClient {
 
   get = async <T>(url: string, config = {}) => {
     const response = await this.client.get<T>(url, config).catch((err) => {
-      throw new Error(err)
+      if (err.response)
+        throw { status: err.response.status, data: err.response.data }
+      throw { status: 500, data: "ECONNREFUSED", error: err }
+      // throw new Error('url:' + url + ' Error:' + err)
     })
     return response.data
   }
 
   getRaw = async <T>(url: string, config = {}) => {
     const response = await this.client.get<T>(url, config).catch((err) => {
-      throw new Error(err)
+      if (err.response)
+        throw { status: err.response.status, data: err.response.data }
+      throw { status: 500, data: "ECONNREFUSED", error: err }
+      // throw new Error('url:' + this.client.getUri() + url + ' ' + err)
     })
     const { data, status } = response
     return { ...data, status }
@@ -27,7 +33,10 @@ export class HttpClient {
 
   post = async <T>(url: string, data: any, config = {}) => {
     const response = await this.client.post<T>(url, data, config).catch((err) => {
-      throw new Error(err)
+      if (err.response)
+        throw { status: err.response.status, data: err.response.data }
+      throw { status: 500, data: "ECONNREFUSED", error: err }
+      // throw new Error('url:' + url + ' Error:' + err)
     })
     return response.data
   }
